bd47ba0c2b7a83c9a0e96b6da6764fdb90c86718
rg98098 <rg98098@gmail.com>
2024-07-27 20:32:51 +0530

    separate data fetch logic from resource loader, it is providing more modular

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import axios from "axios";
 import { ProductInfo } from "./ProductInfo";
 import { ResourceLoader } from "./ResourceLoader";
 import { UserInfo } from "./UserInfo";
+import { DataSource } from "./DataSource";
+
+const getServerData = url => async () => {
+	const response = await axios.get(url);
+	return response.data;
+}
 
 function App() {
 	return (
     <>
-    <ResourceLoader resourceURL="/users/123" resourceName={"user"}>
+    <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
+			<UserInfo />
+		</DataSource>
+    <DataSource getDataFunc={getServerData('/products/2345')} resourceName="product">
+			<ProductInfo />
+		</DataSource>
+
+    {/* <ResourceLoader resourceURL="/users/123" resourceName={"user"}>
       <UserInfo/>
     </ResourceLoader>
     <ResourceLoader resourceURL="/products/2345" resourceName={"product"}>
       <ProductInfo/>
-    </ResourceLoader>
+    </ResourceLoader> */}
     </>
 	);
 }
