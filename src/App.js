f2bcd5a8d9c5263c035784b763fc007125c7b6d2
rg98098 <rg98098@gmail.com>
2024-07-29 20:36:47 +0530

    controlled onboarding flow

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
 import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 
 const StepOne = ({ goToNext }) => (
@@ -9,26 +11,42 @@ const StepOne = ({ goToNext }) => (
 const StepTwo = ({ goToNext }) => (
 	<>
 	<h1>Step 2</h1>
-	<button onClick={() => goToNext({ age: 100 })}>Next</button>
+	<button onClick={() => goToNext({ age: 63 })}>Next</button>
 	</>
 );
 const StepThree = ({ goToNext }) => (
 	<>
 	<h1>Step 3</h1>
+	<p>Congratulations! You qualify for our senior discount</p>
+	<button onClick={() => goToNext({})}>Next</button>
+	</>
+);
+const StepFour = ({ goToNext }) => (
+	<>
+	<h1>Step 4</h1>
 	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
 	</>
 );
 
 function App() {
+	const [onboardingData, setOnboardingData] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(0);	
+
+	const onNext = stepData => {
+		setOnboardingData({ ...onboardingData, ...stepData });
+		setCurrentIndex(currentIndex + 1);
+	}
+
 	return (
-		<UncontrolledOnboardingFlow onFinish={data => {
-			console.log(data);
-			alert('Onboarding complete!');
-		}}>
+		<ControlledOnboardingFlow
+			currentIndex={currentIndex}
+			onNext={onNext}
+		>
 			<StepOne />
 			<StepTwo />
-			<StepThree />
-		</UncontrolledOnboardingFlow>
+			{onboardingData.age >= 62 && <StepThree />}
+			<StepFour />
+		</ControlledOnboardingFlow>
 	);
 }
 
