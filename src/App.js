9925826dc9a980c4a3beee368455cd0c5bb65ae9
rg98098 <rg98098@gmail.com>
2024-07-29 11:53:55 +0530

    uncontrolled form component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import axios from "axios";
-import { ProductInfo } from "./ProductInfo";
-import { ResourceLoader } from "./ResourceLoader";
-import { UserInfo } from "./UserInfo";
-import { DataSource } from "./DataSource";
+import { UncontrolledForm } from "./UncontrolledForm";
 
-const getServerData = url => async () => {
-	const response = await axios.get(url);
-	return response.data;
-}
 
-const getLocalStorageData = key => () =>{
-  return localStorage.getItem(key)
-}
-
-const Text = ({message}) => <h1>{message}</h1>
 
 function App() {
 	return (
     <>
-    <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
-			<UserInfo />
-		</DataSource>
-    <DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
-			<Text />
-		</DataSource>
-
-    {/* <ResourceLoader resourceURL="/users/123" resourceName={"user"}>
-      <UserInfo/>
-    </ResourceLoader>
-    <ResourceLoader resourceURL="/products/2345" resourceName={"product"}>
-      <ProductInfo/>
-    </ResourceLoader> */}
+    <UncontrolledForm/>
     </>
 	);
 }
