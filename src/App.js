6da44f341c77e983799c7c24a466fc303d7c70e9
rg98098 <rg98098@gmail.com>
2024-07-30 10:57:01 +0530

    hoc for just printing props of child from hoc wrapped component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,12 @@
-import { useState } from 'react';
-import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+import { printProps } from "./printProps";
+import { UserInfo } from "./UserInfo";
 
-const StepOne = ({ goToNext }) => (
-	<>
-	<h1>Step 1</h1>
-	<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
-	</>
-);
-const StepTwo = ({ goToNext }) => (
-	<>
-	<h1>Step 2</h1>
-	<button onClick={() => goToNext({ age: 63 })}>Next</button>
-	</>
-);
-const StepThree = ({ goToNext }) => (
-	<>
-	<h1>Step 3</h1>
-	<p>Congratulations! You qualify for our senior discount</p>
-	<button onClick={() => goToNext({})}>Next</button>
-	</>
-);
-const StepFour = ({ goToNext }) => (
-	<>
-	<h1>Step 4</h1>
-	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
-	</>
-);
 
 function App() {
-	const [onboardingData, setOnboardingData] = useState({});
-	const [currentIndex, setCurrentIndex] = useState(0);	
 
-	const onNext = stepData => {
-		setOnboardingData({ ...onboardingData, ...stepData });
-		setCurrentIndex(currentIndex + 1);
-	}
-
+  const UserInfoWrapped = printProps(UserInfo);
 	return (
-		<ControlledOnboardingFlow
-			currentIndex={currentIndex}
-			onNext={onNext}
-		>
-			<StepOne />
-			<StepTwo />
-			{onboardingData.age >= 62 && <StepThree />}
-			<StepFour />
-		</ControlledOnboardingFlow>
+    <UserInfoWrapped a={12} b="Hello" c={{name: "Rahul"}}/>
 	);
 }
 
